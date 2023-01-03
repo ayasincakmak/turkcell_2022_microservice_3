234054bb306b46679b53e51dbdcb3dd22a5f8abf
Hakan ÖZER <hakanozer02@gmail.com>
2023-01-03 09:42:00 +0300

    Product

--- Product/src/main/java/com/works/services/ProductService.java
+++ Product/src/main/java/com/works/services/ProductService.java
@@ -1,8 +1,14 @@
 package com.works.services;
 
+import com.works.entities.Product;
 import com.works.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -10,6 +16,18 @@ public class ProductService {
 
     final ProductRepository productRepository;
 
+    public ResponseEntity save(Product product) {
+        Map<String, Object> hm = new LinkedHashMap<>();
+        hm.put("status", true);
+        hm.put("result", productRepository.save(product));
+        return new ResponseEntity(hm, HttpStatus.OK);
+    }
 
 
+    public ResponseEntity list() {
+        Map<String, Object> hm = new LinkedHashMap<>();
+        hm.put("status", true);
+        hm.put("result", productRepository.findAll());
+        return new ResponseEntity(hm, HttpStatus.OK);
+    }
 }
