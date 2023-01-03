a27c4f3cc2e1f4eda71e5ac439a41c5924718ec5
Hakan ÖZER <hakanozer02@gmail.com>
2023-01-03 07:27:44 +0300

    Product

--- Account/src/main/java/com/works/restcontrollers/AccountRestController.java
+++ Account/src/main/java/com/works/restcontrollers/AccountRestController.java
@@ -6,12 +6,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/account")
 public class AccountRestController {
 
     final AccountService accountService;
