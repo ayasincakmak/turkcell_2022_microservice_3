8dbfa8163e6809fb9ce6a32bc826c8068fdded05
Hakan ÖZER <hakanozer02@gmail.com>
2023-01-03 11:53:34 +0300

    Product

---
+++ Product/src/main/java/com/works/props/AccountData.java
@@ -0,0 +1,57 @@
+
+package com.works.props;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+
+@Generated("jsonschema2pojo")
+public class AccountData {
+
+    private Boolean status;
+    private Result result;
+    private String sessionID;
+    private String server;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Result getResult() {
+        return result;
+    }
+
+    public void setResult(Result result) {
+        this.result = result;
+    }
+
+    public String getSessionID() {
+        return sessionID;
+    }
+
+    public void setSessionID(String sessionID) {
+        this.sessionID = sessionID;
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
---
+++ Product/src/main/java/com/works/props/Result.java
@@ -0,0 +1,48 @@
+
+package com.works.props;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+
+@Generated("jsonschema2pojo")
+public class Result {
+
+    private Integer aid;
+    private String email;
+    private String password;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public Integer getAid() {
+        return aid;
+    }
+
+    public void setAid(Integer aid) {
+        this.aid = aid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
